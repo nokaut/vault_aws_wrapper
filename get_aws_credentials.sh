07a8caa136bedc0edf381d18081b2ba2a86465f8
Marcin Grzybowski <marcin.grzybowski@nokaut.pl>
2022-02-15 20:46:18 +0100

    add option to use role

--- get_aws_credentials.sh
+++ get_aws_credentials.sh
@@ -5,6 +5,11 @@
 # [profile mainprofile]
 # region=eu-west-1
 # credential_process=/usr/local/bin/get_aws_credentials.sh --vault_aws_profile mainprofile --vault_aws_role terraform
+#
+# [profile roleprofile]
+# region=eu-west-1
+# credential_process=/usr/local/bin/get_aws_credentials.sh --vault_aws_profile roleprofile --vault_aws_role administrator --arn_role arn:aws:iam::111111111111:role/admin
+
 
 LOCK_FILE=/tmp/vault_file_session.lock
 CREDENTIALS=/tmp/vault_file_session.session
@@ -12,6 +17,7 @@ TOKEN_ACTIVE=0
 VAULT_AWS_PROFILE=''
 VAULT_AWS_ROLE=''
 VAULT_SESSION_DIR=/tmp
+DURATION_SECONDS=900
 
 parse_args() {
     case "$1" in
@@ -23,6 +29,12 @@ parse_args() {
             ;;
         --vault_session_dir)
             VAULT_SESSION_DIR="$2"
+            ;;
+        --arn_role)
+            AWS_ARN_ROLE="$2"
+            ;;
+        --duration_seconds)
+            DURATION_SECONDS="$2"
             ;;
         *)
             echo "Unknown or badly placed parameter '$1'." 1>&2
@@ -56,11 +68,29 @@ if [ $TOKEN_ACTIVE -eq 0 ]; then
   json_result=$(vault read ${VAULT_AWS_PROFILE}/creds/${VAULT_AWS_ROLE} -format=json)
   echo $(echo $json_result | jq -r ".lease_duration") > $LOCK_FILE
   echo $(date +"%s") >> ${LOCK_FILE}
-  AWS_ACCESS_KEY_ID=$( echo $json_result | jq -r ".data.access_key")
-  AWS_SECRET_ACCESS_KEY=$( echo $json_result | jq -r ".data.secret_key")
-  echo '{ "Version": 1, "AccessKeyId": "'"${AWS_ACCESS_KEY_ID}"'", "SecretAccessKey": "'"${AWS_SECRET_ACCESS_KEY}"'" }' > $CREDENTIALS
+  export AWS_ACCESS_KEY_ID=$( echo $json_result | jq -r ".data.access_key")
+  export AWS_SECRET_ACCESS_KEY=$( echo $json_result | jq -r ".data.secret_key")
+  sleep 10
+
+  if [ -z $AWS_ARN_ROLE ];
+  then
+    echo '{ "Version": 1, "AccessKeyId": "'"${AWS_ACCESS_KEY_ID}"'", "SecretAccessKey": "'"${AWS_SECRET_ACCESS_KEY}"'" }' > $CREDENTIALS
+  else
+    export AWS_SECURITY_TOKEN=$( echo $json_result | jq -r ".data.security_token")
+    STS_RESULT=$(aws sts assume-role --role-arn ${AWS_ARN_ROLE} --role-session-name ${VAULT_AWS_PROFILE}_${VAULT_AWS_ROLE}_${AWS_ACCESS_KEY_ID} --duration-seconds ${DURATION_SECONDS})
+    AWS_ACCESS_KEY_ID=$( echo ${STS_RESULT} | jq -r ".Credentials.AccessKeyId")
+    AWS_SECRET_ACCESS_KEY=$( echo ${STS_RESULT} | jq -r ".Credentials.SecretAccessKey")
+    AWS_SESSION_TOKEN=$(echo ${STS_RESULT} | jq -r ".Credentials.SessionToken")
+    AWS_EXPIRATION_TOKEN=$(echo ${STS_RESULT} | jq -r ".Credentials.Expiration")
+
+    AWS_EXPIRATION_TOKEN_SEC=$(date --utc -d"${AWS_EXPIRATION_TOKEN}" +%s)
+    AWS_EXPIRATION_TOKEN_SEC_DIFF=$(($AWS_EXPIRATION_TOKEN_SEC-$(date --utc +"%s")))
+    echo ${AWS_EXPIRATION_TOKEN_SEC_DIFF} > $LOCK_FILE
+    echo $(date +"%s") >> ${LOCK_FILE}
+    echo '{ "Version": 1, "AccessKeyId": "'"${AWS_ACCESS_KEY_ID}"'", "SecretAccessKey": "'"${AWS_SECRET_ACCESS_KEY}"'", "SessionToken": "'"${AWS_SESSION_TOKEN}"'", "Expiration": "'"${AWS_EXPIRATION_TOKEN}"'" }' > $CREDENTIALS
+  fi
   chmod 600 $CREDENTIALS
-  sleep 15
+  sleep 5
 fi
 
 cat $CREDENTIALS
